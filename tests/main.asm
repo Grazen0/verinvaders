e982dc070acf862f0f9d952641b5363401bfcc8b
José Daniel Grayson <josedanielgrayson@proton.me>
2026-01-27 00:04:03 -0500

    feat: update test program

--- tests/main.asm
+++ tests/main.asm
@@ -1,12 +1,20 @@
 .org 0x0000
 
-    lxi     h, 0x2000
-    lxi     b, 0x0000
-
-    inx     b
-    inx     b
-
     mvi     a, 0x42
-    sbi     0x02
-    inx     b
-    inx     b
+init:
+    add     a
+
+    lxi     h, init
+    pchl
+
+loop:
+    jmp     loop
+
+thing:
+    dcr     l
+    jnz     thing
+
+    mvi     a, 0x69
+    nop
+    nop
+
