a1a9cb01f7e075520c454565544648b3d7008050
José Daniel Grayson <josedanielgrayson@proton.me>
2026-01-31 16:55:39 -0500

    refactor: use clk_divider for all clock divisions

--- src/top.v
+++ src/top.v
@@ -20,6 +20,7 @@ module top (
     output wire audio_out
 );
   wire sys_clk;
+  wire vga_clk;
 
   clk_divider #(
       .PERIOD(50)
@@ -28,31 +29,12 @@ module top (
       .clk_out(sys_clk)
   );
 
-  wire vga_clk;
-
-`ifdef IVERILOG
   clk_divider #(
       .PERIOD(4)
   ) vga_divider (
       .clk_in (clk),
       .clk_out(vga_clk)
   );
-
-`else
-  wire vga_clkfb;
-
-  MMCME2_BASE #(
-      .CLKIN1_PERIOD(10.0),  // 100 MHz input
-      .CLKFBOUT_MULT_F(8.0),
-      .CLKOUT0_DIVIDE_F(32.0)  // 100 * 8 / 32 = 25 MHz
-  ) vga_mmcm (
-      .CLKIN1  (clk),
-      .CLKFBIN (vga_clkfb),
-      .CLKFBOUT(vga_clkfb),
-      .CLKOUT0 (vga_clk),
-      .LOCKED  ()
-  );
-`endif
 
   wire joypad_scl_out;
   wire joypad_sda_in;
